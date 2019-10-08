33caed8a89809541167de23a6db797c4b2b70df1
babamba <orochi13@naver.com>
2019-10-08 18:30:40 +0900

    db property 암호화

--- src/main/java/com/ngBoot/userportal/UserPortalApplication.java
+++ src/main/java/com/ngBoot/userportal/UserPortalApplication.java
@@ -1,12 +1,15 @@
 package com.ngBoot.userportal;
 
+import com.ulisesbocchio.jasyptspringboot.annotation.EnableEncryptableProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@EnableEncryptableProperties
 @SpringBootApplication
 public class UserPortalApplication {
 
 	public static void main(String[] args) {
+		System.setProperty("jasypt.encryptor.password", "jw");
 		SpringApplication.run(UserPortalApplication.class, args);
 	}
 }
---
+++ src/main/resources/JasyptConfig.java
@@ -0,0 +1,20 @@
+@Configuration
+public class JasyptConfig {
+
+    private static final String ENCRYPT_KEY = "jw";
+
+    @Bean("jasyptStringEncrptor")
+    public StringEncryptor stringEncryptor() {
+        PooledPBEStringEncryptor encryptor = new PooledPBEStringEncryptor();
+        SimpleStringPBEConfig config = new SimpleStringPBEConfig();
+        config.setPassword(ENCRYPT_KEY);
+        config.setAlgorithm("PBEWithMD5AndDES");
+        config.setKeyObtentionIterations("1000");
+        config.setPoolSize("1");
+        config.setProviderName("toma");
+        config.setSaltGeneratorClassName("org.jasypt.salt.RandomSaltGenerator");
+        config.setStringOutputType("base64");
+        encryptor.setConfig(config);
+        return encryptor;
+    }
+}
